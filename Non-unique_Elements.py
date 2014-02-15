bfeb2a745b726e79298230efb9fb5b0ab3330b58
Junichi TANAKA <junsan45@gmail.com>
2014-02-16 02:16:48 +0900

    fix assert

--- Non-unique_Elements.py
+++ Non-unique_Elements.py
@@ -2,13 +2,8 @@
 def checkio(list):
   return filter(lambda x: list.count(x) > 1, list)
 
-def myassert(x, y):
-  print(x)
-  if x != y:
-    raise
-
 if __name__ == '__main__':
-  myassert(checkio([1, 2, 3, 1, 3]), [1, 3, 1, 3])
-  myassert(checkio([1, 2, 3, 4, 5]), [])
-  myassert(checkio([5, 5, 5, 5, 5]), [5, 5, 5, 5, 5])
-  myassert(checkio([10, 9, 10, 10, 9, 8]), [10, 9, 10, 10, 9])
+  assert checkio([1, 2, 3, 1, 3]) == [1, 3, 1, 3]
+  assert checkio([1, 2, 3, 4, 5]) == []
+  assert checkio([5, 5, 5, 5, 5]) == [5, 5, 5, 5, 5]
+  assert checkio([10, 9, 10, 10, 9, 8]) == [10, 9, 10, 10, 9]
